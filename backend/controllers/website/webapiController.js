b78b44a4790027c1fd760a3d766a15e05826dedf
Deepak <deepakkushwaha5945@gmail.com>
2026-07-04 16:53:22 +0530

    filter logic added

--- backend/controllers/website/webapiController.js
+++ backend/controllers/website/webapiController.js
@@ -1,78 +1,99 @@
-const blogModel = require("../../models/dashboardModel/blog.model")
-const faqModel = require("../../models/dashboardModel/faq.model")
-const locationModel = require("../../models/dashboardModel/location.model")
-const nutritionMode = require("../../models/dashboardModel/nutrition.mode")
-const programModel = require("../../models/dashboardModel/programModel")
-const testimonialModel = require("../../models/dashboardModel/testimonial.model")
+const blogModel = require("../../models/dashboardModel/blog.model");
+const faqModel = require("../../models/dashboardModel/faq.model");
+const locationModel = require("../../models/dashboardModel/location.model");
+const nutritionMode = require("../../models/dashboardModel/nutrition.mode");
+const programModel = require("../../models/dashboardModel/programModel");
+const testimonialModel = require("../../models/dashboardModel/testimonial.model");
 
 let fetchprogram = async (req, res) => {
-
-    let data = await programModel.find()
-    res.send(
-        {
-            status: true,
-            message: 'program data fetched...',
-            data
-        }
-    )
-}
+  let data = await programModel.find();
+  res.send({
+    status: true,
+    message: "program data fetched...",
+    data,
+  });
+};
 let fetchnutrition = async (req, res) => {
+  let data = await nutritionMode.find();
+  res.send({
+    status: true,
+    message: "nutrition data fetched...",
+    data,
+  });
+};
 
-    let data = await nutritionMode.find()
-    res.send(
-        {
-            status: true,
-            message: 'nutrition data fetched...',
-            data
-        }
-    )
-}
+let filterData = async (req, res) => {
+  const { category, value } = req.query;
+
+  if (category === "category") {
+    let data = await nutritionMode.find({ category: value });
+    return res.send({
+      status: true,
+      message: "nutrition filter data fetched...",
+      data,
+    });
+  } else {
+    if (category === "sort" && value === "A-Z") {
+      let data = await nutritionMode.find().sort({ nutritionName: 1 });
+      return res.send({
+        status: true,
+        message: "nutrition data sort A-Z..",
+        data,
+      });
+    } else {
+      let data = await nutritionMode.find().sort({ nutritionName:-1 });
+      return res.send({
+        status: true,
+        message: "nutrition data sort Z-A...",
+        data,
+      });
+    }
+  }
+
+};
 
 let fetchblog = async (req, res) => {
-    let data = await blogModel.find()
-    res.send(
-        {
-            status: true,
-            message: 'blog data fetched...',
-            data
-        }
-    )
-}
+  let data = await blogModel.find();
+  res.send({
+    status: true,
+    message: "blog data fetched...",
+    data,
+  });
+};
 
 let fetchlocation = async (req, res) => {
-
-    let data = await locationModel.find()
-    res.send(
-        {
-            status: true,
-            message: 'location data fetched...',
-            data
-        }
-    )
-}
+  let data = await locationModel.find();
+  res.send({
+    status: true,
+    message: "location data fetched...",
+    data,
+  });
+};
 
 let fetchfaq = async (req, res) => {
-
-    let data = await faqModel.find()
-    res.send(
-        {
-            status: true,
-            message: 'FAQ data fetched...',
-            data
-        }
-    )
-}
+  let data = await faqModel.find();
+  res.send({
+    status: true,
+    message: "FAQ data fetched...",
+    data,
+  });
+};
 
 let fetchtestimonial = async (req, res) => {
+  let data = await testimonialModel.find();
+  res.send({
+    status: true,
+    message: "testimonial data fetched...",
+    data,
+  });
+};
 
-    let data = await testimonialModel.find()
-    res.send(
-        {
-            status: true,
-            message: 'testimonial data fetched...',
-            data
-        }
-    )
-}
-
-module.exports = { fetchprogram,fetchnutrition,fetchblog,fetchlocation,fetchfaq,fetchtestimonial }
+module.exports = {
+  fetchprogram,
+  fetchnutrition,
+  filterData,
+  fetchblog,
+  fetchlocation,
+  fetchfaq,
+  fetchtestimonial,
+};
--- backend/routes/websRoutes/webApiRoute.js
+++ backend/routes/websRoutes/webApiRoute.js
@@ -1,10 +1,11 @@
 let express = require('express')
-const { fetchprogram, fetchnutrition, fetchblog, fetchlocation, fetchfaq, fetchtestimonial } = require('../../controllers/website/webapiController')
+const { fetchprogram, fetchnutrition, fetchblog, fetchlocation, fetchfaq, fetchtestimonial, filterData } = require('../../controllers/website/webapiController')
 
 let webapiRoutes = express()
 
 webapiRoutes.get('/fetch-program', fetchprogram)
 webapiRoutes.get('/fetch-nutrition', fetchnutrition)
+webapiRoutes.get('/fetch-filterData',filterData)
 webapiRoutes.get('/fetch-blog', fetchblog)
 webapiRoutes.get('/fetch-location', fetchlocation)
 webapiRoutes.get('/fetch-faq', fetchfaq)
